b8eaa64711ade7adb0a9a1f57eb8586adc0bec07
Zach Raines <dev@zmraines.com>
2025-11-23 10:31:42 -0600

    refactor: look up textual widgets with query_one

--- src/kase/tui/query.py
+++ src/kase/tui/query.py
@@ -1,4 +1,5 @@
 import asyncio
+from sqlite3 import Date
 from typing import Unpack, cast, final, override
 
 from rapidfuzz import utils
@@ -39,10 +40,6 @@ class QueryApp(App[str]):
         super().__init__(**kwargs)
 
         self.repo = CaseRepo(case_dir)
-        self.caselist = DataTable[str](
-            cursor_type="row", zebra_stripes=True, classes="caselist"
-        )
-        self.input = Input(placeholder="Filter", compact=True)
         self.filter_text = None
         self.update_task = None
 
@@ -50,15 +47,19 @@ class QueryApp(App[str]):
     def compose(self):
         yield Header()
         with Horizontal():
-            yield self.caselist
+            yield DataTable[str](
+                cursor_type="row", zebra_stripes=True, classes="caselist"
+            )
             yield Markdown(classes="preview")
-        yield self.input
+        yield Input(placeholder="Filter", compact=True)
         yield Footer()
 
     def on_mount(self):
-        _ = self.caselist.add_columns("SF ID", "LP Bug", "Title", "Description")
+        _ = self.query_one(DataTable).add_columns(
+            "SF ID", "LP Bug", "Title", "Description"
+        )
         self._reset_table()
-        _ = self.input.focus()
+        _ = self.query_one(Input).focus()
 
     async def on_data_table_row_highlighted(self, event: DataTable.RowHighlighted):
         case_folder = event.row_key.value
@@ -76,7 +77,7 @@ class QueryApp(App[str]):
     async def _update_case_list(self):
         await asyncio.sleep(0.1)
         selected = self.selected_case()
-        _ = self.caselist.clear()
+        _ = self.query_one(DataTable).clear()
 
         if self.filter_text is None or self.filter_text == "":
             return self._reset_table()
@@ -88,6 +89,7 @@ class QueryApp(App[str]):
             self._add_row(case)
 
     def _apply_filter(self, filter_text: str, selected: str | None):
+        caselist = self.query_one(DataTable)
         for case in self.repo.cases:
             score = (
                 partial_ratio(
@@ -100,31 +102,30 @@ class QueryApp(App[str]):
             if score > 0.8:
                 self._add_row(case)
                 if selected is not None and str(case.path) == selected:
-                    self.caselist.move_cursor(row=self.caselist.get_row_index(selected))
+                    caselist.move_cursor(row=caselist.get_row_index(selected))
         if selected is None:
-            self.caselist.move_cursor(row=0)
+            caselist.move_cursor(row=0)
 
     def selected_case(self) -> str | None:
-        if self.caselist.row_count == 0:
+        caselist = self.query_one(DataTable)
+        if caselist.row_count == 0:
             return None
         # Typechecker doesn't know that Reactive[T] casts to T
         # noinspection PyTypeChecker
-        return self.caselist.coordinate_to_cell_key(
-            self.caselist.cursor_coordinate
-        ).row_key.value
+        return caselist.coordinate_to_cell_key(caselist.cursor_coordinate).row_key.value
 
     def action_cursor_up(self):
-        self.caselist.action_cursor_up()
+        self.query_one(DataTable).action_cursor_up()
 
     def action_cursor_down(self):
-        self.caselist.action_cursor_down()
+        self.query_one(DataTable).action_cursor_down()
 
     def action_select_row(self):
         if case_folder := self.selected_case():
             cast(QueryApp, self.app).exit(case_folder, return_code=0)
 
     def _add_row(self, case: Case):
-        _ = self.caselist.add_row(
+        _ = self.query_one(DataTable).add_row(
             case.sf,
             case.lp,
             case.title,
